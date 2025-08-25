dcea29dfc7ae72987a47d06f72d6a14c7f3843b5
kdgenez <kdgenez@gmail.com>
2025-08-24 19:30:44 -0500

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from langchain_groq import ChatGroq
+
+# Crea el LLM Groq con LangChain
+
+def get_llm(model_name: str, api_key: str):
+    if not api_key:
+        raise RuntimeError("GROQ_API_KEY no proporcionada")
+
+    llm = ChatGroq(
+        temperature=0.2,
+        model_name=model_name,
+        groq_api_key=api_key,
+        max_tokens=None,
+    )
+    return llm
